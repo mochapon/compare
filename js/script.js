489184e735d3f3a8f8b4302e9e1814e95e56be8d
mochapon <77262390+mochapon@users.noreply.github.com>
2021-01-17 23:28:17 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,33 @@
+'use strict';
+
+  // ボタンの要素を取得
+  const button = document.getElementById("button");
+
+  // ボタンをクリックした時の処理
+  button.addEventListener("click", function (e) {
+
+    e.preventDefault();
+
+    // 2つの入力フォームの値を取得
+    const kakaku = document.getElementById("kakaku").value;
+    const guramu = document.getElementById("guramu").value;
+
+    // 2つの数値を除算して100を乗算
+    // const cal = (parseInt(kakaku, 10) / parseInt(guramu, 10)) * 100;
+    const cal = (kakaku / guramu) * 100;
+
+    // a 要素の作成と属性の指定
+    const newTxt = document.createTextNode(cal);
+
+    // li 要素の作成
+    const newLi = document.createElement("li");
+    newLi.appendChild(newTxt);
+
+    // リストに追加
+    const list = document.getElementById("js_resultList");
+    // list.innerText = 'リスト'
+    list.appendChild(newLi);
+    
+
+  });
+
