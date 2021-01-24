2b6616b1e5d37e750803f828db830e572f7f01f7
mochapon <77262390+mochapon@users.noreply.github.com>
2021-01-24 18:33:41 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -8,10 +8,11 @@
     const guramu = document.getElementById("guramu").value;
     const kakaku = document.getElementById("kakaku").value;
     
-    const tanka = (kakaku / guramu) * 100;
+    // 小数第三位以下切り捨て
+    const tanka = Math.floor(((kakaku / guramu) * 100) * 100) / 100;
 
     const li = document.createElement('li');
-    li.textContent = `${guramu}：${kakaku}：＠${tanka}円`;
+    li.textContent = `${guramu}g：${kakaku}円：＠${tanka}円`;
 
     document.querySelector('ul').appendChild(li);
     
